93de580393431dc0f689766f96bd2cc8317ace88
Harsh Mawandia <61311217+DoubleBerserker@users.noreply.github.com>
2024-09-20 22:10:33 +0530

    add: Include Url paths in urls.py and create 'home' and 'prediction' templates

--- slate/core/urls.py
+++ slate/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='index'),
     path('home/', views.home, name='index'),
+    path('prediction/', views.prediction, name='prediction')
 ]
--- slate/core/views.py
+++ slate/core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 def home(request):
-    return HttpResponse("Success")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+
+def prediction(request):
+    template = loader.get_template('prediction.html')
+    return HttpResponse(template.render())
